7549c5e54070c7adbbf5dce948b76b01d4f9201a
Amdirpherian <63004866+Amdirpherian@users.noreply.github.com>
2021-04-24 15:24:28 -0400

    Update CF 1374.py

--- python/CF 1374.py
+++ python/CF 1374.py
@@ -1,7 +1,9 @@
 count = int(input())
-for x in range(count):
+for _ in range(count):
     x,y,n = map(int,input().split())
-    for k in range(n+1,0,-1):
-        if k%x==y:
-            print(k)
-            break
+    if n % x == y:
+        print(n)
+    elif n % x > y:
+        print(n - (n % x) + y)
+    else:
+        print((n - (n % x))-x+y)
