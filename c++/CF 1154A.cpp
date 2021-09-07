9297d340970fdfd6e8687f8b1c51a938ac778b75
Amdirpherian <63004866+Amdirpherian@users.noreply.github.com>
2021-09-07 13:53:08 -0400

    Update CF 1154A.cpp

--- c++/CF 1154A.cpp
+++ c++/CF 1154A.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
 
+    int p,q,r,s;
+
+    cin >> p >> q >> r >> s;
+    vector<int> nums {p,q,r,s};
+
+    int m = max({p,q,r,s});
+
+    for (int n:nums){
+        if (m-n != 0)
+            cout  <<  m-n << " ";
+    }
+
+    return 0;
+}
